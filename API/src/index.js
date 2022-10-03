aed8f8e0258962cbf70a52134b0ea86f17a538ba
João Victor <jvfernandes.contato@gmail.com>
2022-10-03 17:58:03 -0300

    Criando o servidor http com express

--- API/src/index.js
+++ API/src/index.js
@@ -1,8 +1,9 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello World! API</h1>');
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello world!');
 });
 
-server.listen(3000, () => console.log('server started at http://localhost:3000'));
+app.listen(3000,() => console.log('Server started at http://localhost:3000'));

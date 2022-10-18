b9f4ff36c2b6e4abf2eabd987eac4eed7672e3d7
João Victor <jvfernandes.contato@gmail.com>
2022-10-18 10:35:58 -0300

    Middlewares

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,14 @@ const UserController = require('./app/controllers/UserController');
 
 const router = Router();
 
-router.get('/users', UserController.index);
+router.get(
+  '/users',
+  (request, response, next) => {
+    request.appId = 'MeuAppID',
+    next();
+  },
+  UserController.index,
+);
 router.get('/users/:id', UserController.show);
 router.delete('/users/:id', UserController.delete);
 

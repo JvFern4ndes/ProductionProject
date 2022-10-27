7bbdffad26c0060f4b98b0b84e216079990346f9
João Victor <jvfernandes.contato@gmail.com>
2022-10-27 13:09:50 -0300

    Preparando o banco de dados

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE productionproject;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE functions (
+CREATE TABLE IF NOT EXISTS functions (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );

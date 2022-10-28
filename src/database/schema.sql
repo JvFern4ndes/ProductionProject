c878b30eb6e81eb9418f1234477ee8c982759265
João Victor <jvfernandes.contato@gmail.com>
2022-10-28 08:18:56 -0300

    Criando a tabela de machines

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,11 +7,18 @@ CREATE TABLE IF NOT EXISTS functions (
   name VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS machines (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS users (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL,
   email VARCHAR UNIQUE,
   phone VARCHAR,
   function_id UUID,
-  FOREIGN KEY(function_id) REFERENCES functions(id)
+  machine_id UUID,
+  FOREIGN KEY(function_id) REFERENCES functions(id),
+  FOREIGN KEY(machine_id) REFERENCES machines(id)
 );

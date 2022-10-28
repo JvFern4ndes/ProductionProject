4b15d84e479875adce3eb5476f5446ac30f5a88a
João Victor <jvfernandes.contato@gmail.com>
2022-10-28 08:10:49 -0300

    Criando a tabela de users

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS functions (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  function_id UUID,
+  FOREIGN KEY(function_id) REFERENCES functions(id)
+);
